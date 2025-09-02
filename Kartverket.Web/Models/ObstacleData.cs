6aaa63e8fd3266cf17b63106e1cae617a21cbbf5
RaniaEG <79601775+RaniaEG@users.noreply.github.com>
2025-09-02 11:43:14 +0200

    Update ObstacleData.cs
    Validerings regler

--- Kartverket.Web/Models/ObstacleData.cs
+++ Kartverket.Web/Models/ObstacleData.cs
@@ -1,6 +1,14 @@
 public class ObstacleData
  {
-     public string ObstacleName { get; set; }
-     public double ObstacleHeight { get; set; }
-     public string ObstacleDescription { get; set; }
+[Required(ErrorMessage ="Field is required")]
+[MaxLength(100)]
+public string ObstacleName { get; set; }
+
+[Required(ErrorMessage = "Field is required")]
+[Range(0, 200)]
+public double ObstacleHeight { get; set; }
+
+[Required(ErrorMessage = "Field is required")]
+[MaxLength(1000)]
+public string ObstacleDescription { get; set; }
  }
