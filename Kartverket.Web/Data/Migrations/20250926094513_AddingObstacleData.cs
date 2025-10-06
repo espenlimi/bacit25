852a4c215499f566e02f006c88466899c4f97820
Espen Limi <Espen.Limi@aenergi.no>
2025-10-06 14:06:21 +0200

    Added migration

---
+++ Kartverket.Web/Data/Migrations/20250926094513_AddingObstacleData.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kartverket.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingObstacleData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ObstacleDatas",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ObstacleName = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ObstacleHeight = table.Column<double>(type: "double", nullable: false),
+                    ObstacleDescription = table.Column<string>(type: "varchar(1000)", maxLength: 1000, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ObstacleDatas", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ObstacleDatas");
+        }
+    }
+}
--- Kartverket.Web/Data/ObstacleData.cs
+++ Kartverket.Web/Data/ObstacleData.cs
@@ -1,6 +1,8 @@
- public class ObstacleData
- {
-     public string ObstacleName { get; set; }
-     public double ObstacleHeight { get; set; }
-     public string ObstacleDescription { get; set; }
- }
+namespace Kartverket.Web.Data;
+public class ObstacleData
+{
+    public long Id { get; set; }
+    public string ObstacleName { get; set; }
+    public double ObstacleHeight { get; set; }
+    public string ObstacleDescription { get; set; }
+}
